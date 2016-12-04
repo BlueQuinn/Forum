ac18db7e726832556227ffb0126667c07cd94004
Blue <blubluescarlet@gmail.com>
2016-12-04 17:20:54 +0700

    Change package name

--- src/main/java/winter/controller/AndroidLinkController.java
+++ src/main/java/winter/controller/AndroidLinkController.java
@@ -20,7 +20,7 @@ public class AndroidLinkController
                 "  \"relation\": [\"delegate_permission/common.handle_all_urls\"],\n" +
                 "  \"target\": {\n" +
                 "    \"namespace\": \"android_app\",\n" +
-                "    \"package_name\": \"com.example\",\n" +
+                "    \"package_name\": \"com.example.applink\",\n" +
                 "    \"sha256_cert_fingerprints\":\n" +
                 "    [\"14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5\"]\n" +
                 "  }\n" +
